bde68b5a74ad80443d7e7c06fce08edc6d5a35ab
Agnieszka98 <38754510+Agnieszka98@users.noreply.github.com>
2018-05-09 19:53:31 +0200

    Add files via upload

---
+++ zad2_2.R
@@ -0,0 +1,10 @@
+# zadanie 2_2
+today <- Sys.Date()
+deadline <- as.Date("2022-06-30")
+
+hours <- as.numeric(difftime(deadline, today), units="hours")
+additional <- as.numeric(difftime(strptime("23:59", format = "%H:%M"), 
+                                  strptime("13:17", format = "%H:%M")), units="hours") # do końca dzisiaj
+hours <- hours + additional
+
+paste("do końca studiów zostało", hours, "godzin")
