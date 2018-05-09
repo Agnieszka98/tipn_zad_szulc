b64960ffb728b36f201691a1bc6f3138a38886c3
Agnieszka98 <38754510+Agnieszka98@users.noreply.github.com>
2018-05-09 19:54:58 +0200

    Add files via upload

---
+++ zad2_3.R
@@ -0,0 +1,13 @@
+#zadanie 2_3
+
+wszystkie_lata <- c(1800:2018)
+for(i in wszystkie_lata) {
+  if(i %% 4 == 0){
+    if(i %% 100 != 0){
+      print(i)
+    }
+  }
+  else if (i %% 400 == 0){
+    print(i)
+  }
+}
